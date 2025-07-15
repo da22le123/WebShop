a28c34d5539ebb1471ff1af1a0dfc4b031dcbe29
da22le123 <ilia1892006@gmail.com>
2025-07-15 07:56:26 +0200

    added dockerfile, changed ports

--- seed-script.sh
+++ seed-script.sh
@@ -2,7 +2,7 @@
 
 # Seed a user
 echo "Seeding a user..."
-curl -X POST http://localhost:3000/users \
+curl -X POST http://localhost:4000/users \
     -H "Content-Type: application/json" \
     -d '{"username": "user1", "password": "password1"}'
 echo ""
@@ -10,7 +10,7 @@ echo ""
 # Seed 10 products
 echo "Seeding products..."
 for i in {1..10}; do
-    curl -X POST http://localhost:3000/products \
+    curl -X POST http://localhost:4000/products \
         -H "Content-Type: application/json" \
         -d "{\"name\": \"Product $i\", \"price\": $((i * 10))}"
     echo ""
